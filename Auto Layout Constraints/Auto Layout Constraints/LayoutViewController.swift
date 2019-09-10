6cb7ef3198464ba2adb241345b489268bb866d44
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-09-10 04:24:40 -0500

    modified first image layout

--- Auto Layout Constraints/Auto Layout Constraints/LayoutViewController.swift
+++ Auto Layout Constraints/Auto Layout Constraints/LayoutViewController.swift
@@ -9,28 +9,90 @@
 import UIKit
 
 class LayoutViewController: UIViewController {
-    var mainImage: UIImageView?
-
+    
+    let mainImageView: UIImageView = {
+        let mainImage = UIImageView(image: #imageLiteral(resourceName: "mhwMainMenu"))
+        mainImage.translatesAutoresizingMaskIntoConstraints = false
+        mainImage.contentMode = .scaleAspectFit
+        return mainImage
+    }()
     
     
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background.jpg")!)
+//        view.addSubview(infoTextView)
+        
+        layoutFunc()
+        
+    }
+    
+    func layoutFunc() {
+        // container
+        let topImageContainer = UIView()
+        topImageContainer.backgroundColor = .orange
+        view.addSubview(topImageContainer)
+        
+        topImageContainer.translatesAutoresizingMaskIntoConstraints = false
+        
+        topImageContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        
+        topImageContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        topImageContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
         
         
-        let image: UIImage = UIImage(named: "mhwMainMenu.jpg")!
-        mainImage = UIImageView(image: image)
-        self.view.addSubview(mainImage!)
+        topImageContainer.addSubview(mainImageView)
+        mainImageView.centerXAnchor.constraint(equalTo: topImageContainer.centerXAnchor).isActive = true
+        mainImageView.centerYAnchor.constraint(equalTo: topImageContainer.centerYAnchor).isActive = true
+        mainImageView.heightAnchor.constraint(equalTo: topImageContainer.heightAnchor, multiplier: 0.5).isActive = true
+
+
+       
         
-        mainImage?.translatesAutoresizingMaskIntoConstraints = false
+        mainImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        mainImageView.widthAnchor.constraint(equalToConstant: 380).isActive = true
+        mainImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
+        // end of main image code
         
-        mainImage?.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mainImage?.topAnchor.constraint(equalTo: view.topAnchor, constant: 80).isActive = true
-        mainImage?.widthAnchor.constraint(equalToConstant: 380).isActive = true
-        mainImage?.heightAnchor.constraint(equalToConstant: 250).isActive = true
-            
-        //view.addSubview(mainImage)
+        // this is for the texting layout
+        
+//        let textInfo = UITextView(coder: <#T##NSCoder#>)
+//        textInfo.text = "Massive Expansion for Monster Hunter: World"
+//        self.view.addSubview(textInfo)
+//
+//        textInfo.translatesAutoresizingMaskIntoConstraints = false
+//
+//
+//
+//        textInfo.topAnchor.constraint(equalTo: mainImage!.bottomAnchor, constant: 125).isActive = true
+//        textInfo.leftAnchor.constraint(equalTo: mainImage!.leftAnchor).isActive = true
+//        textInfo.rightAnchor.constraint(equalTo: mainImage!.rightAnchor).isActive = true
+//        textInfo.bottomAnchor.constraint(equalTo: mainImage!.bottomAnchor, constant: 0).isActive = true
+      
+        let textView = UITextView(frame: .zero, textContainer: nil)
+        textView.backgroundColor = UIColor.black.withAlphaComponent(0.5) // visual debugging
+        textView.text = "Massive Expansion for Monster Hunter: World"
+        textView.isScrollEnabled = false   // causes expanding height
+        textView.font = UIFont.boldSystemFont(ofSize: 30)
+        textView.textAlignment = .center
+        textView.isEditable = false
+        textView.textColor = UIColor.white
+        
+        view.addSubview(textView)
+        
+        // Auto Layout
+        textView.translatesAutoresizingMaskIntoConstraints = false
+        
+        let safeArea = view.safeAreaLayoutGuide
+        NSLayoutConstraint.activate([
+            textView.topAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -460),
+            textView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
+            textView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
+            ])
+        
+        // end of text layout code
     }
 }
 
