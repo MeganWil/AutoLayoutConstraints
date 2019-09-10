820f2938840280783fd3abb0c3721adccd6197fd
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-09-10 05:40:40 -0500

    finished

--- Auto Layout Constraints/Auto Layout Constraints/LayoutViewController.swift
+++ Auto Layout Constraints/Auto Layout Constraints/LayoutViewController.swift
@@ -18,81 +18,71 @@ class LayoutViewController: UIViewController {
     }()
     
     
+    let textViews: UITextView = {
+        let textView = UITextView()
+            textView.backgroundColor = UIColor.black.withAlphaComponent(0.5) // visual debugging
+            textView.translatesAutoresizingMaskIntoConstraints = false
+        textView.text = "Massive Expansion for Monster Hunter: World \n Available now!"
+            textView.isScrollEnabled = false
+            textView.font = UIFont.boldSystemFont(ofSize: 20)
+            textView.textAlignment = .center
+            textView.isEditable = false
+            textView.textColor = UIColor.white
+        return textView
+    }()
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
         self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background.jpg")!)
-//        view.addSubview(infoTextView)
-        
+        view.addSubview(textViews)
+    
         layoutFunc()
         
     }
     
+    
+    
     func layoutFunc() {
         // container
         let topImageContainer = UIView()
-        topImageContainer.backgroundColor = .orange
+//        topImageContainer.backgroundColor = .orange
         view.addSubview(topImageContainer)
         
         topImageContainer.translatesAutoresizingMaskIntoConstraints = false
         
-        topImageContainer.topAnchor.constraint(equalTo: view.topAnchor).isActive = true
+        topImageContainer.topAnchor.constraint(equalTo: view.topAnchor, constant: 0).isActive = true
         
         topImageContainer.leadingAnchor.constraint(equalTo: view.leadingAnchor).isActive = true
+        
         topImageContainer.trailingAnchor.constraint(equalTo: view.trailingAnchor).isActive = true
+        topImageContainer.heightAnchor.constraint(equalTo: view.heightAnchor, multiplier: 0.5).isActive = true
         
         
         topImageContainer.addSubview(mainImageView)
+        
         mainImageView.centerXAnchor.constraint(equalTo: topImageContainer.centerXAnchor).isActive = true
         mainImageView.centerYAnchor.constraint(equalTo: topImageContainer.centerYAnchor).isActive = true
-        mainImageView.heightAnchor.constraint(equalTo: topImageContainer.heightAnchor, multiplier: 0.5).isActive = true
-
-
-       
+        mainImageView.heightAnchor.constraint(equalTo: topImageContainer.heightAnchor, multiplier: 0.8).isActive = true
         
+
+
+        // start of main image code
         mainImageView.centerXAnchor.constraint(equalTo: view.centerXAnchor).isActive = true
-        mainImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 100).isActive = true
+        mainImageView.topAnchor.constraint(equalTo: view.topAnchor, constant: 50).isActive = true
         mainImageView.widthAnchor.constraint(equalToConstant: 380).isActive = true
         mainImageView.heightAnchor.constraint(equalToConstant: 250).isActive = true
         // end of main image code
         
+        
         // this is for the texting layout
-        
-//        let textInfo = UITextView(coder: <#T##NSCoder#>)
-//        textInfo.text = "Massive Expansion for Monster Hunter: World"
-//        self.view.addSubview(textInfo)
-//
-//        textInfo.translatesAutoresizingMaskIntoConstraints = false
-//
-//
-//
-//        textInfo.topAnchor.constraint(equalTo: mainImage!.bottomAnchor, constant: 125).isActive = true
-//        textInfo.leftAnchor.constraint(equalTo: mainImage!.leftAnchor).isActive = true
-//        textInfo.rightAnchor.constraint(equalTo: mainImage!.rightAnchor).isActive = true
-//        textInfo.bottomAnchor.constraint(equalTo: mainImage!.bottomAnchor, constant: 0).isActive = true
-      
-        let textView = UITextView(frame: .zero, textContainer: nil)
-        textView.backgroundColor = UIColor.black.withAlphaComponent(0.5) // visual debugging
-        textView.text = "Massive Expansion for Monster Hunter: World"
-        textView.isScrollEnabled = false   // causes expanding height
-        textView.font = UIFont.boldSystemFont(ofSize: 30)
-        textView.textAlignment = .center
-        textView.isEditable = false
-        textView.textColor = UIColor.white
-        
-        view.addSubview(textView)
-        
-        // Auto Layout
-        textView.translatesAutoresizingMaskIntoConstraints = false
-        
-        let safeArea = view.safeAreaLayoutGuide
-        NSLayoutConstraint.activate([
-            textView.topAnchor.constraint(equalTo: safeArea.bottomAnchor, constant: -460),
-            textView.leadingAnchor.constraint(equalTo: safeArea.leadingAnchor),
-            textView.trailingAnchor.constraint(equalTo: safeArea.trailingAnchor)
-            ])
-        
-        // end of text layout code
+        textViews.topAnchor.constraint(equalTo: topImageContainer.bottomAnchor,constant: 20).isActive = true
+        textViews.leftAnchor.constraint(equalTo: mainImageView.leftAnchor).isActive = true
+        textViews.rightAnchor.constraint(equalTo: mainImageView.rightAnchor).isActive = true
+
     }
 }
 
