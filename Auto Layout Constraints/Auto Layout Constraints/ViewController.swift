214b74fd28549fdb4b8b285be03f100d19d01bf0
Megan Wilson <mlw7v3@mail.missouri.edu>
2019-09-10 00:16:00 -0500

    Added files

---
+++ Auto Layout Constraints/Auto Layout Constraints/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Auto Layout Constraints
+//
+//  Created by Megan Wilson on 9/9/19.
+//  Copyright © 2019 Megan Wilson. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.view.backgroundColor = UIColor(patternImage: UIImage(named: "Background.jpg")!)
+    }
+}
+
